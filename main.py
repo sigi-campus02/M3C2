44a1b6ea9557ba581990d632a544578acfc082b7
sigi-campus02 <147090809+sigi-campus02@users.noreply.github.com>
2025-09-01 19:49:13 +0200

    Add main entry point test and delay CLIApp import

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ Run Pipeline using the command line:
     Option 1. With arguments in config.json file: python -m main
     Option 2. Directly with arguments in the command line:
 
-                python -m main 
-                --data_dir ./data  
-                --folders 0342-0349  
-                --filename_ref ref.ply  
-                --filename_mov mov.ply  
-                --project MARS 
-                --output_format excel 
-                --outlier_detection_method rmse 
+                python -m main
+                --data_dir ./data
+                --folders 0342-0349
+                --filename_ref ref.ply
+                --filename_mov mov.ply
+                --project MARS
+                --output_format excel
+                --outlier_detection_method rmse
                 --outlier_multiplicator 3.0
                 --log_level INFO
 """
 
-from m3c2.cli.cli import CLIApp
-
 
 def main() -> None:
     """Execute the command line application."""
+    from m3c2.cli.cli import CLIApp
+
     CLIApp().run()
 
 
