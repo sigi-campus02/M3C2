8fd435b9ec4e7487006ec8cfeecb2409e0fa4516
sigi-campus02 <147090809+sigi-campus02@users.noreply.github.com>
2025-09-03 20:40:00 +0200

    Expose ReportBuilder in visualization package

--- m3c2/visualization/__init__.py
+++ m3c2/visualization/__init__.py
@@ -2,5 +2,6 @@
 
 from .services.plot_service import PlotService
 from .services.plot_comparedistances_service import PlotServiceCompareDistances
+from .services.report_service import ReportBuilder
 
-__all__ = ["PlotService", "PlotServiceCompareDistances"]
+__all__ = ["PlotService", "PlotServiceCompareDistances", "ReportBuilder"]
